99c0db5128cd1e61a2eb56377695f69fed1af683
Baranyi Gábor <90318277+bgabesz1@users.noreply.github.com>
2021-10-17 19:09:59 +0200

    órai feladatok befejezve

--- first updated/src/main/java/hu/me/iit/webtalk/first/ArticleRepositoryMemoryImpl.java
+++ first updated/src/main/java/hu/me/iit/webtalk/first/ArticleRepositoryMemoryImpl.java
@@ -8,47 +8,50 @@ import java.util.ArrayList;
 public class ArticleRepositoryMemoryImpl implements ArticleRepository{
     private final List<ArticleDto> articles = new ArrayList<>();
 
-        private int findArticleById(Long id) {
-            int found = -1;
-            for (int i = 0; i < articles.size(); i++) {
-                if (articles.get(i).getTitle().equals(id)) {
-                    found = i;
-                    break;
-                }
-            }
-            return found;
-        }
-
-        @Override
-        public List<ArticleDto> findAll() {
-            return articles;
-        }
-
-        @Override
-        public ArticleDto getById(Long id) {
-            return null;
-        }
-
-        @Override
-        public Long save(ArticleDto articleDto) {
-            int found = findArticleById(articleDto.getId());
-
-            if (found != -1) {
-                ArticleDto foundArticle = articles.get(found);
-                foundArticle.setAuthor(articleDto.getAuthor());
-                foundArticle.setPages(articleDto.getPages());
-            } else {
-                articles.add(articleDto);
-            }
-            return null;
-        }
-
-        @Override
-        public void deleteById(Long id) {
-            int found = findArticleById(id);
-
-            if (found != -1) {
-                articles.remove(found);
+    public int findArticleById(Long id) {
+        int found = -1;
+        for (int i = 0; i < articles.size(); i++) {
+            if (articles.get(i).getId().equals(id)) {
+                found = i;
+                break;
             }
         }
+        return found;
+    }
+
+    @Override
+    public List<ArticleDto> findAll() {
+        return articles;
+    }
+
+    @Override
+    public ArticleDto getById(Long id) {
+        int articleByIdIndex = findArticleById(id);
+
+        return articleByIdIndex == -1 ? null : articles.get(articleByIdIndex);
+    }
+
+    @Override
+    public Long save(ArticleDto articleDto) {
+        int found = findArticleById(articleDto.getId());
+
+        if (found != -1) {
+            ArticleDto foundArticle = articles.get(found);
+            foundArticle.setAuthor(articleDto.getAuthor());
+            foundArticle.setPages(articleDto.getPages());
+            foundArticle.setTitle(articleDto.getTitle());
+        } else {
+            articles.add(articleDto);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        int found = findArticleById(id);
+
+        if (found != -1) {
+            articles.remove(found);
+        }
+    }
 }
--- first updated/src/main/java/hu/me/iit/webtalk/first/MainController.java
+++ first updated/src/main/java/hu/me/iit/webtalk/first/MainController.java
@@ -8,54 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="article/")
-public class MainController {
-	private final List<ArticleDto> articles=new ArrayList<>();
 
+@RequestMapping(path="article")
+public class MainController {
+
+	private final ArticleService articleService;
+
+	public MainController(ArticleService articleService) {
+		this.articleService = articleService;
+	}
 
 	@GetMapping(path="", produces= MediaType.APPLICATION_JSON_VALUE)
-	List<ArticleDto> allArticles() {
-		return articles;
+	public List<ArticleDto> allArticles() {
+		return articleService.findAll();
 	}
 
 	@PostMapping(path="")
 	public void newArticle(@RequestBody @Valid ArticleDto articleDto) {
-		articles.add(articleDto);
+		articleService.save(articleDto);
 	}
 
-	private int findArticleById(String id) {
-		int found = -1;
-		for (int i = 0; i < articles.size(); i++) {
-			if (articles.get(i).getTitle().equals(id)) {
-				found = i;
-				break;
-			}
-		}
-		return found;
+	@PutMapping(path="/")
+	public void replaceArticle(@RequestBody @Valid ArticleDto articleDto) {
+		articleService.save(articleDto);
 	}
 
-	@PutMapping(path = "{id}")
-	public void ReplaceArticle (@Valid @PathVariable("id") String id, @RequestBody @Valid ArticleDto articleDto){
-		int found=findArticleById(id);
-
-		if (found != -1) {
-			ArticleDto foundArticle = articles.get(found);
-			foundArticle.setAuthor(articleDto.getAuthor());
-			foundArticle.setPages(articleDto.getPages());
-		}
+	@DeleteMapping (path="/{id}")
+	public void deleteArticle(@PathVariable("id") Long id) {
+		articleService.deleteById(id);
 	}
 
-	@DeleteMapping(path="{id}")
-	public void DeleteArticle(@PathVariable("id") String id) {
-		int found=-1;
-		for (int i=0; i<articles.size(); i++){
-			if(articles.get(i).getTitle().equals(id)){
-				found=i;
-				break;
-			}
-		}
-		if (found != -1){
-			articles.remove(found);
-		}
-	}
 }
