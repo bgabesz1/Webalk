201cca9f672d8fd672efe5f743e912c3f4fa0758
Baranyi Gábor <90318277+bgabesz1@users.noreply.github.com>
2021-10-24 19:39:53 +0200

    részfeladat

--- Dependency/src/main/java/hu/me/iit/webalk/dependency/DependencyImpl.java
+++ Dependency/src/main/java/hu/me/iit/webalk/dependency/DependencyImpl.java
@@ -1,16 +1,20 @@
 package hu.me.iit.webalk.dependency;
 
-public class DependencyImpl implements Dependency {
-    @Override
-    public void helpNoParameterNoReturnValue() {}
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+    @Component
+    public class DependencyImpl implements Dependency {
+        @Override
+        public void helpNoParameterNoReturnValue() {
+        }
 
-    @Override
-    public int helpNoParameterReturnValue() {
-        return 45;
+        @Override
+        public int helpNoParameterReturnValue() {
+            return 45;
+        }
+
+        @Override
+        public void helpWithParameterNoReturnValue(int a) {
+
+        }
     }
-
-    @Override
-    public void helpWithParameterNoReturnValue(int a) {
-
-    }
-}
--- Dependency/src/main/java/hu/me/iit/webalk/dependency/ServiceImpl.java
+++ Dependency/src/main/java/hu/me/iit/webalk/dependency/ServiceImpl.java
@@ -1,5 +1,8 @@
 package hu.me.iit.webalk.dependency;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ServiceImpl implements Service {
     private final Dependency dependency;
 
@@ -22,7 +25,7 @@ public class ServiceImpl implements Service {
     }
 
     public void callHelpAndPassParameter5() {
-         dependency.helpWithParameterNoReturnValue(5);
+        dependency.helpWithParameterNoReturnValue(5);
     }
 
 }
