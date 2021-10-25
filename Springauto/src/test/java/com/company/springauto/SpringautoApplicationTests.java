d8a6ceeef16a7694ed92e788e437c363ccabe0e7
Baranyi Gábor <90318277+bgabesz1@users.noreply.github.com>
2021-10-25 23:36:28 +0200

    STRINGAUTO

---
+++ Springauto/src/test/java/com/company/springauto/SpringautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringautoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
