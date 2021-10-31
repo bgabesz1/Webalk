f36a16e162c371cffc36c2d6ab215b76d1968f2d
Baranyi Gábor <90318277+bgabesz1@users.noreply.github.com>
2021-10-31 09:48:57 +0100

    Delete db directory

--- db/src/main/java/hu/me/iit/uni/db/PeopleDto.java
+++
@@ -1,7 +0,0 @@
-package hu.me.iit.uni.db;
-
-public class PeopleDto {
-    private long id;
-    private String name;
-    private int age;
-}
--- db/src/main/java/hu/me/iit/uni/db/PeopleRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.me.iit.uni.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public class PeopleRepository extends PagingAndSortingRepository<People, Long>{
-}
